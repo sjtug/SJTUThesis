b26399e329c5469ed5a81a80378d3610baf33602
Jianwen Wei - acer <wei.jianwen@gmail.com>
2010-12-02 21:34:38 +0800

    将页眉中内容用变量代替。

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 BASE=diss
 
+rm *.pdf
 xelatex -no-pdf --interaction=nonstopmode ${BASE}
 bibtex ${BASE}
 xelatex -no-pdf --interaction=nonstopmode ${BASE}
 xelatex --interaction=nonstopmode ${BASE}
+evince diss.pdf
 
