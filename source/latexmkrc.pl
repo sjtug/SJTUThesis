3ac70fd2bac70ee4417a056da9eda856ea3ccd9a
Alexara Wu <alexarawu@outlook.com>
2019-07-25 15:47:48 +0800

    Always try to embed fonts

--- source/latexmkrc.pl
+++ source/latexmkrc.pl
@@ -4,6 +4,9 @@
 $pdf_mode = 5;
 $postscript_mode = $dvi_mode = 0;
 
+# Always try to embed fonts, ignoring licensing flags, etc.
+$xdvipdfmx = 'xdvipdfmx -E -o %D %O %S';
+
 # Files to clean.
 $clean_ext = 'bbl glo gls hd loa run.xml thm xdv';
 
