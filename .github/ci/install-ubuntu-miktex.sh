23674ea44d623745c14b0ef3bdbf0be16f39bda9
Alex Chi <iskyzh@gmail.com>
2021-04-22 15:50:11 +0800

    disable cache for miktex

--- .github/ci/install-ubuntu-miktex.sh
+++ .github/ci/install-ubuntu-miktex.sh
@@ -7,7 +7,7 @@ export REPO=https://www.ctan.org/tex-archive/systems/win32/miktex
 
 # Install MiKTeX
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
-echo "deb ${REPO}/setup/deb/ focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+echo "deb http://miktex.org/download/ubuntu focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
 sudo apt-get update -qq
 sudo apt-get install -y --no-install-recommends ghostscript miktex
 
