50063b344c4493ab1f1d1bbb68500e841e6d75e0
Alex Chi <iskyzh@gmail.com>
2021-05-24 16:34:38 +0800

    add three-part-table with footnote (#640)

--- .github/ci/install-ubuntu-texlive.sh
+++ .github/ci/install-ubuntu-texlive.sh
@@ -63,6 +63,7 @@ tlmgr install           \
   sourcesanspro         \
   tex-gyre              \
   threeparttable        \
+  threeparttablex       \
   tocloft               \
   translator            \
   trimspaces            \
