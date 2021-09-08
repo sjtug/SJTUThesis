bcbe2365ed4253cc0625f32e1593e2973cd2d78b
Alex Chi <iskyzh@gmail.com>
2021-09-08 20:34:43 +0800

    fix no fiile produced
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- sjtuthesis/build.lua
+++ sjtuthesis/build.lua
@@ -10,26 +10,10 @@ docfiledir       = "doc"
 
 typesetexe       = "xelatex"
 typesetfiles     = {"sjtuthesisuserguide.tex","sjtuthesis.dtx"}
-
+typesetdemofiles = {}
 bibtexexe        = "bibtex"
 
 unpackexe        = "xetex"
-unpackopts       = "--interaction=batchmode"
 unpackfiles      = {"sjtuthesis.dtx"}
 
 packtdszip       = true
-
--- function type_example_latexmk()
---   local file = "../"..docfiledir.."/sjtuthesisuserguide.tex"
---   local command = "latexmk -xelatex "..file
---   print("** Running: "..command)
---   errorlevel = run(unpackdir, command)
---   if errorlevel ~= 0 then
---     error("** Error!!: "..command)
---     return errorlevel
---   end
---   return 0
--- end
-
--- specialtypesetting = { }
--- specialtypesetting["sjtuthesisuserguide.tex"] = {func = type_example_latexmk}
