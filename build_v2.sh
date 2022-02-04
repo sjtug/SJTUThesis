20a034b022dc7ef937f81123c3d4eac6fc838695
Alex Chi <iskyzh@gmail.com>
2022-02-04 17:55:53 +0800

    update script
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- build_v2.sh
+++ build_v2.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-# Before running this script, clone v2 of SJTUTeX into project root
+SJTUTEX_DIR="SJTUTeX-v2"
 
-cd SJTUTeX && make generate && cd ..
+if [ ! -d "${SJTUTEX_DIR}" ]; then
+    git clone https://github.com/sjtug/SJTUTeX -b v2 ${SJTUTEX_DIR}
+fi
+
+cd ${SJTUTEX_DIR} && make generate && cd ..
 rm -rf texmf/tex/latex/sjtuthesis
-ln -s $(pwd)/SJTUTeX/sjtuthesis/build/unpacked texmf/tex/latex/sjtuthesis
-make
+ln -s $(pwd)/${SJTUTEX_DIR}/sjtuthesis/build/unpacked texmf/tex/latex/sjtuthesis
+
+latexmk dev-v2.tex -xelatex -time -file-line-error -halt-on-error -interaction=nonstopmode
